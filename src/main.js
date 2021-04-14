f620035fa6b806302dc1df77eba8c0664fb8a8a8
Anlqn <lequangngocan@gmail.com>
2021-04-14 14:41:25 +0700

    anlqn update code

--- src/main.js
+++ src/main.js
@@ -3,12 +3,30 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
+import { store } from './store/store'
+import Axios from 'axios'
+import { BootstrapVue } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+require('@/assets/css/main.css')
+require('@/assets/css/util.css')
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
+Vue.use(BootstrapVue)
+
+Vue.prototype.$http = Axios
+const token = localStorage.getItem('token')
+if (token) {
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+}
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
   template: '<App/>'
