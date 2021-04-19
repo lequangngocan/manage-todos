ca1d977e3ad6bac673747f53c796aeb73d092e25
Anlqn <lequangngocan@gmail.com>
2021-04-19 15:25:25 +0700

    anlqn update class css

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="container-login100">
-    <router-link :to="{name:'Register'}" class="p-t-20 p-r-20 p-b-20 p-l-20">
+    <router-link :to="{name:'Register'}" class="p-4">
       <div class="container-login100-form-btn">
         <button class="login100-form-btn">
           Register
         </button>
       </div>
     </router-link>
-    <router-link :to="{name:'Login'}" class="p-t-20 p-r-20 p-b-20 p-l-20">
+    <router-link :to="{name:'Login'}" class="p-4">
       <div class="container-login100-form-btn">
         <button class="login100-form-btn">
           Login
